188f0d32a2370533ad9d30225f85c9ac42b04d59
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-06-27 22:10:17 +0530

    server

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const users = require('./MOCK_DATA.json')
 const fs = require('fs')
 
 const app = express();
 const PORT = 8000;
 
+const userSchema = new mongoose.Schema({
+    first_name:{
+        type: String,
+        required: true
+    },
+    last_name:{
+        type: String,
+        // required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+});
+
 //Middleware - Plugin
 // This plugin is used to parse the body of the request by creating json object
 app.use(express.urlencoded({extended: false}))
+
+//Creating own middleware
+app.use((req,res,next)=>{
+    console.log("Middleware is working");
+    // return res.json({ msg: "Hello from middleware"});
+    next();
+})
+
+app.use((req,res,next) =>{
+    fs.appendFile('log.txt',` ${Date.now()} Request Method: ${req.method} Request URL: ${req.url} \n`,(err)=>{
+        if(err){
+            console.log(err);
+        }
+        next();
+    })
+})
+
+//Custom headers
+app.get('/api/users', (req, res) => {
+    res.setHeader('X-Custom-Header', 'Hello');
+    //Always add X to custom headers (Just a good practice )
+    return res.json(users);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
@@ -38,11 +79,14 @@ app.get('/api/users', (req, res) => {
 //Post
 app.post("/api/users", (req, res) => {
     const body = req.body;
+    if(!body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({ status: "error", message: "Please provide all the details" });
+    }
     users.push({...body,id: users.length+1});
     console.log(body);
     // return res.json({ status: "pending" });
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
-        return res.json({ status: "success", id: users.length });
+        return res.status(201).json({ status: "success", id: users.length });
     })
     
 });
